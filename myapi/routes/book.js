e5ee56a870aee9986cc56d2e88200f66b182097b
valGe-lang <74594863+valGe-lang@users.noreply.github.com>
2021-02-21 19:52:23 +0200

    crud operaatiot book-taululle tehty ja testattu

---
+++ myapi/routes/book.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const book = require('../models/book_model');
+
+router.get('/:id?',
+ function(request, response) {
+  if (request.params.id) {
+    book.getById(request.params.id, function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        console.log(dbResult[0]);
+        response.json(dbResult[0]);
+      }
+    });
+  } else {
+    book.getbook(function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        console.log(dbResult);
+        response.json(dbResult);
+      }
+    });
+  }
+});
+
+
+router.post('/', 
+function(request, response) {
+  book.add(request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbReuslt);
+    }
+  });
+});
+
+
+router.delete('/:id', 
+function(request, response) {
+  book.delete(request.params.id, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+
+router.put('/:id', 
+function(request, response) {
+  book.update(request.params.id, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+module.exports = router;
